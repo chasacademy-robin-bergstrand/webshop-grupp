60690bd24f4376f9a3ff61b1e0de41ff3090b291
Aran Asaiesh <aran.asaiesh@chasacademy.se>
2023-10-10 14:19:44 +0200

    added color and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#EEEDE7",
+        darkGray: "#414754",
+        lightGray: "#B9B7BD",
+        btnMain: "#09476F",
+        peach: "#F6B68A",
+        btnRemove: "#88232B",
+      },
+      fontFamily: {
+        poppins: ["font-family: 'Poppins', sans-serif;"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
