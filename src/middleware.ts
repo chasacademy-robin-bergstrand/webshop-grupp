2fbc458a7c4ebb8b6040515b9524f51fee61cc86
ChasAcademy-Fernando <115954598+ChasAcademy-Fernando@users.noreply.github.com>
2023-10-30 14:13:54 +0100

    middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,16 @@ import { authMiddleware} from "@clerk/nextjs";
 
 
 export default authMiddleware({
-    publicRoutes:["/","/search","/register","/signin"]
+    publicRoutes:["/","/search","/register","/signin","/products","/api/trpc/product.search",
+    "/admin",
+    "/api/trpc/product.createProduct",
+    "/api/trpc/product.getAll",
+    "/api/trpc/product.delete",
+    "/api/trpc/category.getAll",
+    "/api/trpc/category.create",
+    "/api/trpc/product.getAll,category.getAll",
+
+]
 });;
 
 export const config = {
